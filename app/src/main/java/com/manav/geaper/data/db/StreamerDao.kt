aa788af84991fd45a94feb5e4bdbb51cdd9fb828
manavchaudhary1 <manavchaudhary041@gmail.com>
2026-03-14 00:55:03 +0530

    Changing DB

--- app/src/main/java/com/manav/geaper/data/db/StreamerDao.kt
+++ app/src/main/java/com/manav/geaper/data/db/StreamerDao.kt
@@ -13,19 +13,17 @@ interface StreamerDao {
     @Insert
     suspend fun insert(streamer: Streamer)
 
-    @Query("""
-        UPDATE streamers
-        SET status = :status
-        WHERE username = :username AND site = :site
-    """)
+    @Query("UPDATE streamers SET status = :status WHERE username = :username AND site = :site")
     suspend fun updateStatus(site: String, username: String, status: String)
 
     @Query("""
         UPDATE streamers
-        SET autoRecord = :autoRecord, ffmpegPresetId = :ffmpegPresetId
+        SET autoRecord = :autoRecord,
+            ffmpegPresetId = :ffmpegPresetId,
+            formatSelector = :formatSelector
         WHERE id = :id
     """)
-    suspend fun updateSettings(id: Int, autoRecord: Boolean, ffmpegPresetId: Int?)
+    suspend fun updateSettings(id: Int, autoRecord: Boolean, ffmpegPresetId: Int?, formatSelector: String)
 
     @Delete
     suspend fun delete(streamer: Streamer)
