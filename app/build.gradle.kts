76b9962471481a894d20ea0e29b09700174572fd
manavchaudhary1 <manavchaudhary041@gmail.com>
2026-04-05 18:02:14 +0530

    Signing and Java home update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
 
   signingConfigs {
     create("release") {
-      storeFile = file(System.getenv("KEYSTORE_PATH") ?: "keystore.jks")
+      storeFile = file(System.getenv("KEYSTORE_PATH") ?: "keystore/geaper-release.jks")
       storePassword = System.getenv("KEYSTORE_PASSWORD")
       keyAlias = System.getenv("KEY_ALIAS")
       keyPassword = System.getenv("KEY_PASSWORD")
